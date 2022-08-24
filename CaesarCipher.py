1c698c3c1a854f48fd3f8eb88df665b9115d4c4e
Aadhavan <63637385+adenosinetp10@users.noreply.github.com>
2022-08-24 15:16:42 +0530

    Create CaesarCipher.py

---
+++ CaesarCipher.py
@@ -0,0 +1,20 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+def encrypt(string, key):
+    encString = ""
+    for i in string:
+        encString += ' ' if i == ' ' else alphabet[(alphabet.index(i) + 3) % 26]
+    return encString
+
+def decrypt(string, key):
+    decString = ""
+    for i in string:
+        decString += ' ' if i == ' ' else alphabet[(alphabet.index(i) - 3) % 26]
+    return decString
+
+print("Enter plain text : ")
+t = input()
+e = encrypt(t, 3)
+print("Encrypted text : ", e)
+d = decrypt(e, 3)
+print("Decrypted text : ", d)
